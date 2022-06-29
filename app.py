98571c05b3af64eebcd7397363d98b79b46a6ae1
JHDsBR <Jesse2013.jsc@gmail.com>
2022-06-29 00:17:48 -0300

    second init

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
     return "Está no ar"
+
+# @app.route("/tmp1")
+# def Tmp():
+#     with open("Temp/", "w")
+#     return "Está no ar"
+
+@app.route("/checktmp")
+def CheckTmp():
+    return str(os.path.isdir("tem"))
